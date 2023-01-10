4522e911f599fded4de28844b93b5636e023bee4
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-01-10 03:17:09 -0500

    Initial Commit

---
+++ ViewConstruction/ViewConstructionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewConstructionApp.swift
+//  ViewConstruction
+//
+//  Created by Sergey Lukaschuk on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct ViewConstructionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
