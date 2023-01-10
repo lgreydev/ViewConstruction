a3ffbb0334f45a013581ec80e014130997432ba0
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-01-10 03:38:55 -0500

    Add Taps counter

--- ViewConstruction/ContentView.swift
+++ ViewConstruction/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                counter += 1
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .background(Color(.tertiarySystemFill))
+                    .cornerRadius(5)
+            }
+            
+            if counter > 0 {
+                Text("You've tapped \(counter) times")
+            } else {
+                Text("You've not yet tapped")
+            }
         }
-        .padding()
     }
 }
 
