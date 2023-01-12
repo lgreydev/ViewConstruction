1631003b70e7bd4c81010549f817147c0a09f363
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2023-01-12 03:38:48 -0500

    Use switch

--- ViewConstruction/ContentView.swift
+++ ViewConstruction/ContentView.swift
@@ -20,17 +20,45 @@ struct ContentView: View {
                     .cornerRadius(5)
             }
             
+            /*
             if counter > 0 {
                 Text("You've tapped \(counter) times")
             } else {
                 Text("You've not yet tapped")
             }
+            */
+            
+            switch counter > 0 {
+            case true: Text("You've tapped \(counter) times")
+            case false: Text("You've not yet tapped")
+            }
+            
+            HStack {
+                switch counter > 0 {
+                case true: Rectangle()
+                case false: Circle()
+                }
+            }
+            .padding()
+            .frame(width: 100, height: 100, alignment: .center)
+            .foregroundColor(Color.gray)
+            
         }.debug()
     }
 }
 
 extension View {
     func debug() -> Self {
+        let arr = [10, 20]
+        
+        let reflection = Mirror(reflecting: arr)
+        let children = reflection.children
+        
+        for chail in children {
+            print("label: \(String(describing: chail.label))")
+            print("label: \(chail.value)")
+        }
+        
         print(Mirror(reflecting: self).subjectType)
         return self
     }
